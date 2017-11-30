c0b9558d1585d65028771a8eed75d4751c9e3d2f
myskam7 <mksmflppv98@gmail.com>
2017-11-29 23:49:22 -0500

    edited php

--- submit_form.php
+++ submit_form.php
@@ -1,24 +1,5 @@
 <?php
 
-// If you are using Composer
-require 'vendor/autoload.php';
-
-// If you are not using Composer (recommended)
-// require("path/to/sendgrid-php/sendgrid-php.php");
-
-$from = new SendGrid\Email(null, "test@example.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "mksmflppv98@gmail.com");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-
-$apiKey = getenv('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');
-$sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
 if(isset($_POST['contactFrmSubmit']) && !empty($_POST['name']) && !empty($_POST['email']) && (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) && !empty($_POST['message'])){
     
     // Submitted form data
